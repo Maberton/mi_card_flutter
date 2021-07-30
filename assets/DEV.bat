c11957a077527f92aaaae4d515170aed3dc26aa7
Marcus Berton <marcus.berton@gmail.com>
2021-07-30 02:17:38 -0300

    complete mi card app from dr angela yu course

---
+++ assets/DEV.bat
@@ -0,0 +1,2 @@
+git config --global user.name "Marcus Berton"
+git config --global user.email marcus.berton@gmail.com
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,87 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/images/perfil.jpg'),
+              ),
+              Text(
+                'Marcus Berton',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'flutter developer'.toUpperCase(),
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'OpenSans',
+                  letterSpacing: 2.0,
+                  wordSpacing: 6.0,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 250.0,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    '+55 11 96992-6717',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'OpenSans',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    'marcus.berton@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'OpenSans',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
